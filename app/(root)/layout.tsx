349dab79a43d96dd0bb594f5cf9eb8cd337ac356
meluiz <me.luizfelipe@gmail.com>
2026-02-13 21:47:33 -0300

    feat(layout.tsx): enhance layout with header and footer components for better structure and navigation
    style(layout.tsx): add background gradient and adjust body styles for improved aesthetics

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,6 @@
+import Link from 'next/link';
+
+import { GrupoPlanMark, PlanMark } from '@/components/layout';
 import { exo, openSans } from '@/configs/fonts';
 
 import './globals.css';
@@ -7,12 +10,55 @@ const Layout = (props: Layout.Props) => {
 
   return (
     <html lang="pt-BR" className={`${exo.variable} ${openSans.variable}`}>
-      <body className="relative block min-h-min w-full font-normal font-sans text-base antialiased">
+      <body className="relative block min-h-min w-full font-normal font-sans text-base antialiased bg-white">
+        <span
+          className="w-screen h-[128dvh] block fixed inset-0 overflow-hidden pointer-events-none select-none bg-linear-[50deg] from-[#F5821F] via-[#FFD565] to-[#F5821F]"
+          aria-hidden
+        >
+          <span
+            className="size-full absolute block pointer-events-none select-none z-0 absolute bg-linear-to-b from-accent to-accent/0"
+            aria-hidden
+          />
+        </span>
         <div
           id="__APP_ROOT"
-          className="relative isolate flex h-dvh w-full flex-col z-1 text-black bg-white"
+          className="relative isolate flex h-dvh w-full flex-col z-1 text-black"
         >
-          {children}
+          <div className="@container min-h-dvh flex flex-col space-y-5">
+            <div className="w-full flex-1 relative mx-auto @max-7xl:px-6 max-w-container ">
+              <header className="w-full flex items-center justify-center md:justify-start min-h-content py-10 md:py-13">
+                <Link
+                  href="/"
+                  className="inline-flex items-center justify-center *:[svg]:h-12 md:*:[svg]:h-14.5"
+                  aria-label="Página inicial"
+                >
+                  <PlanMark />
+                </Link>
+                {/*{filterElement}*/}
+              </header>
+              <main className="relative isolate flex w-full flex-col pb-14 md:pb-18">
+                {children}
+              </main>
+            </div>
+            <footer className="w-full relative isolate pt-3.5 pb-5.5 bg-black">
+              <div className="w-full relative mx-auto @max-7xl:px-6 max-w-container">
+                <div className="w-full flex flex-col gap-8 md:flex-row items-center md:items-end justify-between">
+                  <Link
+                    href="https://www.planmkt.com.br/"
+                    className="inline-flex items-center justify-center *:[svg]:h-18 md:*:[svg]:h-23"
+                    rel="noreferrer"
+                    target="_blank"
+                    aria-label="Grupo Plan Marketing"
+                  >
+                    <GrupoPlanMark />
+                  </Link>
+                  <p className="text-xs font-bold text-white font-open-sans">
+                    Grupo Plan Marketing © Todos os direitos reservados - 2025
+                  </p>
+                </div>
+              </div>
+            </footer>
+          </div>
         </div>
       </body>
     </html>
