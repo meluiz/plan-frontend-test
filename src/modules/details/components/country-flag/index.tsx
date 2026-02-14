d3aa9dbd3cac8916d605d4351b5ee6af57bf2637
meluiz <me.luizfelipe@gmail.com>
2026-02-14 18:58:34 -0300

    feat(details): add CountryFlag component to display country flags and update exports for components
    This change introduces a new CountryFlag component that utilizes the Next.js Image component to render country flags. It also updates the index files to export the new component along with existing ones, improving the modular structure of the details module.

---
+++ src/modules/details/components/country-flag/index.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+
+import type { Country } from '@/domains/countries';
+
+export type CountryFlagProps = {
+  country: Country;
+};
+
+export const CountryFlag = async (props: CountryFlagProps) => {
+  const { country } = props;
+
+  return (
+    <div className="flex flex-col items-center gap-y-2.5 col-span-12 lg:col-span-3">
+      <div className="w-full max-w-2xs flex items-center justify-center">
+        <Image
+          src={country.flags.svg}
+          className="w-full aspect-4/3 object-contain object-center"
+          draggable={false}
+          width={20}
+          height={18}
+          alt={country.flags.alt}
+        />
+      </div>
+      <p className="hidden lg:inline-block text-lg text-gray-dark font-bold">Bandeira</p>
+    </div>
+  );
+};
