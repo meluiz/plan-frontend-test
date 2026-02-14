74e10e799861e785fc5168fc22f5aaf4b3274edd
meluiz <me.luizfelipe@gmail.com>
2026-02-14 16:35:43 -0300

    feat(button): add new button component with styles and variants for better UI consistency
    This commit introduces a new button component along with its styles,
    allowing for a more consistent and customizable user interface. The
    button component is built using Tailwind CSS variants for easy
    styling and theming.

---
+++ src/components/ui/button/button.styles.ts
@@ -0,0 +1,11 @@
+import { tv } from 'tailwind-variants';
+
+export const root = tv({
+  base: [
+    'h-11 relative isolate inline-flex cursor-pointer items-center justify-center gap-x-2 whitespace-nowrap rounded-full px-[calc(--spacing(4)-1px)] py-[calc(--spacing(1.5)-1px) outline-hidden transition-all duration-150 ease-in-out focus-visible:ring-2 focus-visible:ring-stroke focus-visible:ring-offset-1 disabled:opacity-50 bg-accent',
+  ],
+});
+
+export const label = tv({
+  base: ['truncate font-black text-base italic text-white'],
+});
---
+++ src/components/ui/button/button.tsx
@@ -0,0 +1,28 @@
+import { ark, type HTMLArkProps } from '@ark-ui/react';
+import type { VariantProps } from 'tailwind-variants';
+
+import { label, root } from './button.styles';
+
+/* --------------------------------------------------------------------------------
+ * Button - Root
+ * -------------------------------------------------------------------------------- */
+
+export type RootProps = RootBaseProps & HTMLArkProps<'button'>;
+export type RootBaseProps = VariantProps<typeof root>;
+
+export const Root = (props: RootProps) => {
+  const { className, ...rest } = props;
+  return <ark.button className={root({ className })} {...rest} />;
+};
+
+/* --------------------------------------------------------------------------------
+ * Button - Label
+ * -------------------------------------------------------------------------------- */
+
+export type LabelProps = LabelBaseProps & HTMLArkProps<'span'>;
+export type LabelBaseProps = VariantProps<typeof label>;
+
+export const Label = (props: LabelProps) => {
+  const { className, ...rest } = props;
+  return <ark.span className={label({ className })} {...rest} />;
+};
