dd6355435f507aea944a1ed2b30273b431571778
meluiz <me.luizfelipe@gmail.com>
2026-02-14 16:42:20 -0300

    feat(page.tsx): implement country list and pagination on the root page to enhance user experience with dynamic content

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,20 @@
-const Page = () => {
+import { getCountryList } from '@/domains/countries';
+import { CountryList, CountryPagination } from '@/modules/home';
+import { searchParams } from '@/utils/helpers';
+
+const Page = async (props: Page.Props) => {
+  const params = await props.searchParams;
+  const parsedSearchParams = searchParams.parse(params);
+
+  const countries = await getCountryList(parsedSearchParams);
+
+  const page = params.page || 1;
+  const list = countries.slice((page - 1) * 8, page * 8);
+
   return (
-    <div>
-      <h1>Welcome to the Root Page</h1>
+    <div className="w-full flex flex-col gap-y-13 relative isolate">
+      <CountryList collection={list} />
+      <CountryPagination totalItems={countries.length} />
     </div>
   );
 };
